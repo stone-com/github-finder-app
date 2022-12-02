0df95d22a898e977f8076e981e53f03208a0761f
Stone Comstock <comstockstone@yahoo.com>
2022-12-01 17:28:25 -0800

    Home, about, and not found components and routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
-import { screen } from '@testing-library/react';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
-        <main className='container mx-auto px-3 pb-12'>Content</main>
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/about' element={<About />}></Route>
+            <Route path='/notfound' element={<NotFound />}></Route>
+            <Route path='/*' element={<NotFound />}></Route>
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
---
+++ src/pages/About.jsx
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details.
+      </p>
+      <p>
+        This project is created by Stone Comstock with the help of Brad Traversy
+        on
+        <a href='https://www.youtube.com/@TraversyMedia'> YouTube</a> and
+        <a href='https://www.udemy.com/user/brad-traversy/'> Udemy</a>
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+    </div>
+  );
+};
+
+export default About;
